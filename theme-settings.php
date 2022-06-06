0c39a76a958fe4f24385dea1f1b105897672e2f1
Sergey Belyaev <sergey.belyaev@p2h.com>
2022-06-06 18:10:13 +0300

    theme-settings.php up

--- theme-settings.php
+++ theme-settings.php
@@ -3,6 +3,11 @@
 use Drupal\Core\Form\FormStateInterface;
 
 function d8base_form_system_theme_settings_alter(&$form, FormStateInterface $form_state) {
+  $form['d8base_settings'] = [
+    '#type'          => 'details',
+    '#title'         => t('Custom theme settings'),
+    '#open'          => TRUE,
+  ];
   $form['d8base_settings']['copyright'] = [
     '#type'          => 'textfield',
     '#title'         => t('Copyright'),
